3dc6732be73356e5663e337cc9eb85c9cc3a3209
Gediminas Urbonas <gediminas@ito.lt>
2018-12-04 20:38:48 +0200

    Initial Commit

---
+++ DanskebankTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DanskebankTest
+//
+//  Created by Gediminas Urbonas on 04/12/2018.
+//  Copyright © 2018 Gediminas Urbonas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
